a9e0e7f839c047af25efdbc9a640c79d19c3c69d
SungbaeYoon <25133924+terrysyoon@users.noreply.github.com>
2021-10-17 06:15:37 +0000

    .

--- inheritance/level/main.cpp
+++ inheritance/level/main.cpp
@@ -21,20 +21,19 @@ public:
     int P_Pub;
     void printP(){
         cout << "World!\n";
+        GP_print_Pub();
         GrandParent::GP_print_Pub(); //OK! Private으로 받아도 class Specifier 붙이면 호출이 되네요.. 이유가 궁금합니다.
         GrandParent::GP_print_Prtd(); //OK!
         //GrandParent::GP_print_Prv(); Error! Declaration이 private이면 안되긴 하네요.
     }
 
-    void GP_print_pub(){ //GP_print_pub은 current scope에서 inaccessible인데, 이 function declaration도 overriding이라 할 수 있을까요?
-        cout << "GP_Pub overriden?!\n";
-    }
+
 
 protected:
     int P_Prtd;
 private:
     int P_Prv;
-    int GP_Prv; //ok. GP_Prv is not accesible on this scope
+    int GP_Prv; //ok. GP_Prv is not accesible on this scope, therefore is not re-declaration.
 };
 
 class Child: public Parent{
@@ -42,10 +41,13 @@ public:
     int C_Pub;
     void printC(){
         cout << "HiHi\n";
-        //GrandParent::GP_print_Pub(); Error! Child inherits Parent with public specifier, so guess this must work, but why not?
-        //Parent::GP_print_Pub(); Error! Why is this an error? line 24 works..
-        //현재 상황(Access Control 등..)에서 Child에서 GP_print_pub();을 call 할 수 있는 방법은 없는 것인가요?
+        //현재 상황(Inheritance, member Access Control 등..)에서 Child에서 GP_print_pub();을 call 할 수 있는 방법은 없는 것인가요?
     }
+
+    void GP_print_pub(){ //GP_print_pub은 current scope에서 inaccessible인데, 이 function declaration도 overriding이라 할 수 있을까요?
+        cout << "GP_Pub overriden?!\n";
+    }
+    
 protected:
     int C_Prtd;
 private:
@@ -57,7 +59,7 @@ int main(){
     Parent *P = new Parent;
     Child *C = new Child;
 
-    P->GrandParent::GP_print_Pub(); //why this is impossible when line 24 is okay?
+    //P->GrandParent::GP_print_Pub(); //why this is impossible when line 24 is okay?
 
     return 0;
 }
